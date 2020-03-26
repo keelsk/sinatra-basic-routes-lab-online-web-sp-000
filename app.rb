275fbfa0be53ff7ec1ff6151acf953bec2a61db6
Kirsten Keels <keelsk@gmail.com>
2020-03-26 17:10:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "Hello, World!"
+  end
+  
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/' do
+    "Hello, World!"
+  end
 end
